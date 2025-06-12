90f123713a03653fdcbfc21b3e303d521d2dddd6
Cass <cassbrady85@gmail.com>
2025-06-11 22:24:47 -0700

    Simplify vite.config.js for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command === 'build') {
-    // Replace 'your-repository-name' with your actual GitHub repo name
-    config.base = '/your-repository-name/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
